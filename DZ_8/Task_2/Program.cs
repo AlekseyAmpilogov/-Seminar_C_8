ee5c7532e799f514a1ffcd79306d69b0ef6055e0
Aleksey <pilot-1981@yandex.ru>
2022-09-27 09:39:22 +0500

    add DZ_8_Task_2.2

--- DZ_8/Task_2/Program.cs
+++ DZ_8/Task_2/Program.cs
@@ -57,26 +57,3 @@ void ProductMatrix(int[,] firstMatrix, int[,] secondMatrix, int [,] resultMatrix
         }
     }
 }
-// int SumLineElements(int[,] array, int i)
-// {
-//     int sumLine = array[i, 0];
-//     for (int j = 1; j < array.GetLength(1); j++)
-//     {
-//         sumLine += array[i, j];
-//     }
-//     return sumLine;
-// }
-
-// int minSumLine = 0;
-// int sumLine = SumLineElements(array, 0);
-// for (int i = 1; i < array.GetLength(0); i++)
-// {
-//     int tempSumLine = SumLineElements(array, i);
-//     if (sumLine > tempSumLine)
-//     {
-//         sumLine = tempSumLine;
-//         minSumLine = i;
-//     }
-// }
-
-// Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
