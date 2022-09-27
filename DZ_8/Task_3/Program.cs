ef4b29c43efd5b0e59b26e446f6b787357625a74
Aleksey <pilot-1981@yandex.ru>
2022-09-27 10:43:14 +0500

    add DZ_8_Task_3.1

---
+++ DZ_8/Task_3/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("\nСлучайный двумерный массив из неповторяющихся двузначных чисел: \n");
+
+int strsCount = 5;
+int columnsCount = 10;
+int[,] array = new int[strsCount, columnsCount];
+
+FillArray(array);
+PrintArray(array);
+void FillArray(int[,] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            while (array[i, j] == 0)
+            {
+                int number = rand.Next(10, 100);
+                if (IsNumberInArray(array, number) == false)
+                {
+                    array[i, j] = number;
+                }
+            }
+        }
+    }
+}
+
+bool IsNumberInArray(int[,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number) return true;
+        }
+    }
+    return false;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(" {0} ", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
