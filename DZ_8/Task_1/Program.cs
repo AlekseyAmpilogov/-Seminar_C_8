f08e4d6ee219f6f94c48802117e9a5a224240185
Aleksey <pilot-1981@yandex.ru>
2022-09-27 08:49:48 +0500

    add DZ_8_Task_1.1

---
+++ DZ_8/Task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿int strsCount = 4;
+int columnsCount = 4;
+Random rnd = new Random();
+int[,] array = new int[strsCount, columnsCount];
+Console.WriteLine("\nСлучайный массив:");
+
+FillArray(array);
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+
+PrintArray(array);
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0} ", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(array, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
